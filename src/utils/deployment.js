b9d780204f007d3f331b869e40d865c26a0f2958
Martin Küchler <mkuechle@hs-mittweida.de>
2019-08-28 13:46:09 +0200

    added commands for compiling and running a special parity

--- src/utils/deployment.js
+++ src/utils/deployment.js
@@ -10,17 +10,27 @@ const deployBlockHashRegistry = async (web3, privateKey) => {
     const bin = JSON.parse(fs.readFileSync('build/contracts/BlockhashRegistry.json', 'utf8'))
 
     const nonce = await web3.eth.getTransactionCount(ethAcc.address)
+
+    const gasPrice = await web3.eth.getGasPrice()
     const transactionParams = {
         from: ethAcc.address,
         data: bin.bytecode,
         gas: 7000000,
         nonce: nonce,
-        gasPrice: await web3.eth.getGasPrice(),
+        gasPrice: gasPrice,
         to: ''
     }
 
     const signedTx = await web3.eth.accounts.signTransaction(transactionParams, ethAcc.privateKey);
     const tx = await (web3.eth.sendSignedTransaction(signedTx.rawTransaction));
+
+    console.log("------------------")
+    console.log("blockhashRegistry")
+    console.log("deployed by:", ethAcc.address)
+    console.log("gasUsed:", tx.gasUsed)
+    console.log("costs", web3.utils.toBN(tx.gasUsed).mul(web3.utils.toBN(gasPrice)).div(web3.utils.toBN('1000000000000000000')).toString('hex') + " ether")
+    console.log("blockhashRegistry-address: " + tx.contractAddress)
+    console.log("------------------")
     return tx
 }
 
@@ -35,12 +45,14 @@ const deployNodeRegistry = async (web3, blockHashRegistryAddress, privateKey) =>
     const blockBefore = await web3.eth.getBlock('latest')
 
     const nonce = await web3.eth.getTransactionCount(ethAcc.address)
+    const gasPrice = await web3.eth.getGasPrice()
+
     const transactionParams = {
         from: ethAcc.address,
         data: bin.bytecode + web3.eth.abi.encodeParameters(['address'], [bhAddress]).substr(2),
         gas: blockBefore.gasLimit,
         nonce: nonce,
-        gasPrice: await web3.eth.getGasPrice(),
+        gasPrice: gasPrice,
         to: ''
 
     }
@@ -48,6 +60,14 @@ const deployNodeRegistry = async (web3, blockHashRegistryAddress, privateKey) =>
     const signedTx = await web3.eth.accounts.signTransaction(transactionParams, ethAcc.privateKey);
 
     const tx = await (web3.eth.sendSignedTransaction(signedTx.rawTransaction));
+
+    console.log("------------------")
+    console.log("nodeRegistry")
+    console.log("deployed by:", ethAcc.address)
+    console.log("gasUsed:", tx.gasUsed)
+    console.log("costs", web3.utils.toBN(tx.gasUsed).mul(web3.utils.toBN(gasPrice)).div(web3.utils.toBN('1000000000000000000')).toString('hex') + " ether")
+    console.log("nodeRegistry-address: " + tx.contractAddress)
+    console.log("------------------")
     return tx
 }
 
