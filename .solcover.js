1d63a40ad6c4109521a7d15fc0cbeae0edf3961d
Martin Küchler <mkuechle@hs-mittweida.de>
2019-10-23 15:48:06 +0200

    coverage working again

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
     testrpcOptions: '--account="0x4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7,4000000000000000000000000000000000" --gasPrice=0x00',
     copyPackages: ['openzeppelin-solidity'],
-
+    skipFiles: ['IERC20.sol']
 };
