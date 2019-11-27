a766e4ef7bb6c5a4ac3c875a17d716861bbc0105
junaidtaj <junaid.taj@slock.it>
2019-11-27 20:30:50 +0100

    test for Node doesnt exist in whitelist

--- test/WhiteList.js
+++ test/WhiteList.js
@@ -140,4 +140,32 @@ contract('WhiteList', async () => {
         //try to reregister and it should fail
         assert.false(await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: txData }, ethAcc.privateKey).catch(_ => false))
     })
+
+    it("Removal non whitelist should fail", async () => {
+        //test for Node doesnt exist in whitelist.
+        
+        const ethAcc = await web3.eth.accounts.privateKeyToAccount("0x4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7");
+
+        //deploy contracts
+        const dataConDeployTx = await deployment.deployNodeRegistryData(web3, ethAcc.privateKey)
+        const logicConDeployTx = await deployment.deployNodeRegistryLogic(web3,undefined, dataConDeployTx.contractAddress, ethAcc.privateKey)
+        const whiteListConDeployTx = await deployment.deployWhiteListContract(web3,logicConDeployTx.contractAddress, ethAcc.privateKey)
+
+        //init contract obj
+        const whiteListCon = new web3.eth.Contract(IN3WhiteList.abi, whiteListConDeployTx.contractAddress)
+
+        //register a node in white list
+        const nodeAddr = "0x71c24b85086928930f5dC2a6690574E7016C1A7F"
+        const txData = whiteListCon.methods.whiteListNode(nodeAddr).encodeABI()
+        await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: txData }, ethAcc.privateKey)
+
+        //confirm node is registered correctly
+        const whiteListAddr = await whiteListCon.methods.getWhiteList().call()
+        assert.equal(nodeAddr.toLowerCase(),whiteListAddr.toLowerCase())
+
+        //now remove node that is different from registered one
+        const tx2Data = whiteListCon.methods.removeNode("0x41d8A416301f53a3EBa3c85B2b88270f636DBd5C").encodeABI()
+        assert.false(await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: tx2Data }, ethAcc.privateKey).catch(_ => false))
+    })
+
 })
