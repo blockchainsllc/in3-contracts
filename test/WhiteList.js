7bc1475722dae8850a7d58d6074607e97f844101
junaidtaj <junaid.taj@slock.it>
2019-11-27 20:27:30 +0100

    test for register whitelist and get correct address

---
+++ test/WhiteList.js
@@ -0,0 +1,38 @@
+const IN3WhiteList = artifacts.require("IN3WhiteList")
+
+const utils = require('../src/utils/utils')
+const deployment = require('../src/utils/deployment')
+const in3Common = require("in3-common")
+const fs = require('fs')
+const Web3 = require('web3')
+
+const ethUtil = require('ethereumjs-util')
+
+
+contract('WhiteList', async () => {
+
+
+    it("Should register whitelist and get correct address", async () => {
+
+        const ethAcc = await web3.eth.accounts.privateKeyToAccount("0x4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7");
+
+        //deploy contracts
+        const dataConDeployTx = await deployment.deployNodeRegistryData(web3, ethAcc.privateKey)
+        const logicConDeployTx = await deployment.deployNodeRegistryLogic(web3,undefined, dataConDeployTx.contractAddress, ethAcc.privateKey)
+        const whiteListConDeployTx = await deployment.deployWhiteListContract(web3,logicConDeployTx.contractAddress, ethAcc.privateKey)
+
+        //init contract obj
+        const whiteListCon = new web3.eth.Contract(IN3WhiteList.abi, whiteListConDeployTx.contractAddress)
+
+        //register a node in white list
+        const nodeAddr = "0x71c24b85086928930f5dC2a6690574E7016C1A7F"
+        const txData = whiteListCon.methods.whiteListNode(nodeAddr).encodeABI()
+        await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: txData }, ethAcc.privateKey)
+
+        //check if node is registered
+        const whiteListAddr = await whiteListCon.methods.getWhiteList().call()
+        assert.equal(nodeAddr.toLowerCase(),whiteListAddr.toLowerCase())
+    })
+
+
+})
