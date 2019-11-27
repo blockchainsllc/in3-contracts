ba879cb1dd21d1276c7c4fe3ac20f367846c497f
junaidtaj <junaid.taj@slock.it>
2019-11-27 20:29:02 +0100

    test for allow node removal from whitelist to owner

--- test/WhiteList.js
+++ test/WhiteList.js
@@ -55,4 +55,34 @@ contract('WhiteList', async () => {
 
     })
 
+    it("Should allow node removal from whitelist to owner", async () => {
+
+        const ethAcc = await web3.eth.accounts.privateKeyToAccount("0x4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7");
+
+        //deploy contracts
+        const dataConDeployTx = await deployment.deployNodeRegistryData(web3, ethAcc.privateKey)
+        const logicConDeployTx = await deployment.deployNodeRegistryLogic(web3,undefined, dataConDeployTx.contractAddress, ethAcc.privateKey)
+        const whiteListConDeployTx = await deployment.deployWhiteListContract(web3,logicConDeployTx.contractAddress, ethAcc.privateKey)
+
+        //init contract obj
+        const whiteListCon = new web3.eth.Contract(IN3WhiteList.abi, whiteListConDeployTx.contractAddress)
+
+        //register a node in white list
+        const nodeAddr = "0x71c24b85086928930f5dC2a6690574E7016C1A7F"
+        const txData = whiteListCon.methods.whiteListNode(nodeAddr).encodeABI()
+        await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: txData }, ethAcc.privateKey)
+
+        //confirm node is registered correctly
+        const whiteListAddr = await whiteListCon.methods.getWhiteList().call()
+        assert.equal(nodeAddr.toLowerCase(),whiteListAddr.toLowerCase())
+
+        //now remove node
+        const tx2Data = whiteListCon.methods.removeNode(nodeAddr).encodeABI()
+        await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: tx2Data }, ethAcc.privateKey)
+
+        //as node is removed it should be empty
+        const whiteListAddrCall2 = await whiteListCon.methods.getWhiteList().call()
+        assert.equal(whiteListAddrCall2,null)
+    })
+
 })
