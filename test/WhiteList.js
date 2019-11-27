9ddb1beb58232095dcf9b8a9b3bc675093794cc9
junaidtaj <junaid.taj@slock.it>
2019-11-27 20:30:16 +0100

    Should not allow to register if node already registered

--- test/WhiteList.js
+++ test/WhiteList.js
@@ -115,4 +115,29 @@ contract('WhiteList', async () => {
         const whiteListAddrCall2 = await whiteListCon.methods.getWhiteList().call()
         assert.equal(nodeAddr.toLowerCase(),whiteListAddrCall2.toLowerCase())
     })
+
+    it("Should not allow to register if node already registered", async () => {
+
+        const ethAcc = await web3.eth.accounts.privateKeyToAccount("0x4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7");
+
+        //deploy contracts
+        const dataConDeployTx = await deployment.deployNodeRegistryData(web3, ethAcc.privateKey)
+        const logicConDeployTx = await deployment.deployNodeRegistryLogic(web3,undefined, dataConDeployTx.contractAddress, ethAcc.privateKey)
+        const whiteListConDeployTx = await deployment.deployWhiteListContract(web3,logicConDeployTx.contractAddress, ethAcc.privateKey)
+
+        //init contract obj
+        const whiteListCon = new web3.eth.Contract(IN3WhiteList.abi, whiteListConDeployTx.contractAddress)
+
+        //register a node in white list
+        const nodeAddr = "0x71c24b85086928930f5dC2a6690574E7016C1A7F"
+        const txData = whiteListCon.methods.whiteListNode(nodeAddr).encodeABI()
+        await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: txData }, ethAcc.privateKey)
+
+        //check if node is registered
+        const whiteListAddr = await whiteListCon.methods.getWhiteList().call()
+        assert.equal(nodeAddr.toLowerCase(),whiteListAddr.toLowerCase())
+
+        //try to reregister and it should fail
+        assert.false(await utils.handleTx({ to: whiteListConDeployTx.contractAddress, data: txData }, ethAcc.privateKey).catch(_ => false))
+    })
 })
